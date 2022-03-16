9e617df22ce458dd1b0c4f9493d57d300a44fba7
JYBDDD <98305534+JYBDDD@users.noreply.github.com>
2022-03-16 10:30:08 +0900

    1

--- Assets/Resources/Script/Manager/GameManager.cs
+++ Assets/Resources/Script/Manager/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : Singleton<GameManager>
     protected override void Awake()
     {
         base.Awake();
+        InsertDataSetting();
     }
 
     /// <summary>
@@ -16,5 +17,31 @@ public class GameManager : Singleton<GameManager>
     private FullDataCollection fullData = new FullDataCollection();
 
 
-    // 데이터 FullDataCollection 삽입부 여기에 만들 것임 TODO
+    #region 데이터 삽입 및 변환부
+    /// <summary>
+    /// FullDataCollection 에 데이터 삽입
+    /// </summary>
+    private void InsertDataSetting()
+    {
+        // 플레이어 데이터
+        var PlayerData = JsonConvertData<UsePlayerData>("Player");
+        // TODO 데이터가 배열이 아닌것같은데..? 확인해보고 하나만 들어가면 수정하기
+
+        // 성장 데이터
+
+
+
+        // 몬스터 데이터
+    }
+
+    /// <summary>
+    /// Json 데이터 변환
+    /// </summary>
+    private T JsonConvertData<T>(string path)
+    {
+        var textAssetData = Resources.Load<TextAsset>($"Document/Json/{path}");
+
+        return JsonUtility.FromJson<T>(textAssetData.ToString());
+    }
+    #endregion
 }
