abb98509db8b0f5ebbc98355e9c35a26cd03b275
Duolines21 <magnic21@naver.com>
2022-03-16 16:51:21 +0900

    작성한거 딱히 없음..

--- Assets/Resources/Script/Manager/GameManager.cs
+++ Assets/Resources/Script/Manager/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LitJson;
+using System.IO;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -13,7 +15,7 @@ public class GameManager : Singleton<GameManager>
     /// <summary>
     /// 총괄 데이터
     /// </summary>
-    public static FullDataCollection FullData { get => Instance.fullData; set => value = Instance.fullData; }
+    public static FullDataCollection FullData { get => Instance.fullData;}
     private FullDataCollection fullData = new FullDataCollection();
 
 
@@ -24,8 +26,10 @@ public class GameManager : Singleton<GameManager>
     private void InsertDataSetting()
     {
         // 플레이어 데이터
-        var PlayerData = JsonConvertData<UsePlayerData>("Player");
-        // TODO 데이터가 배열이 아닌것같은데..? 확인해보고 하나만 들어가면 수정하기
+        //var playerData = JsonConvertData<UsePlayerData>("Player");
+        TextAsset textAsset = Resources.Load<TextAsset>("Document/Json/Player");
+        
+        // TODO jsonUtility는 배열일시 못가져옴, Litjson을 사용해야할듯
 
         // 성장 데이터
 
@@ -34,14 +38,79 @@ public class GameManager : Singleton<GameManager>
         // 몬스터 데이터
     }
 
-    /// <summary>
+    IEnumerator LoadJsonFile(string path)   // 여기 작성중 TODO
+    {
+        string jsonStr = File.ReadAllText(Application.dataPath + $"/Resources/Document/Json/{path}");
+        JsonData jsonData = JsonMapper.ToObject(jsonStr);
+        //Parsing
+
+        yield return null;
+    }
+
+    /*/// <summary>
     /// Json 데이터 변환
     /// </summary>
     private T JsonConvertData<T>(string path)
     {
-        var textAssetData = Resources.Load<TextAsset>($"Document/Json/{path}");
+        Dictionary<int, Dictionary<string, string>> dataDic = new Dictionary<int, Dictionary<string, string>>();    // 데이터 목록
+        Dictionary<string, string> varNameDic = new Dictionary<string, string>(); // 변수(int,float 등..), 변환된 변수 값
 
-        return JsonUtility.FromJson<T>(textAssetData.ToString());
+        string textAssetData = Resources.Load<TextAsset>($"Document/Json/{path}").ToString();
+        string[] lines = textAssetData.Split('[', ']', '{', '}'); // 모두 자르기
+
+        int startVarNameIndex = 0;
+        int lastVarNameIndex = 0;
+        string varName = "";
+
+        int startValueIndex = 0;
+        int lastValueIndex = 0;
+        string valueName = "";
+
+        for (int i = 0; i < lines.Length; ++i)
+        {
+            lines[i] = lines[i].Replace(" ", "");  // 모든 공백 제거
+            lines[i] = lines[i].Replace("\"", "");  // 모든 쌍따음표 제거
+
+            if (lines[i].Contains(":"))
+            {
+                startVarNameIndex = lines[i].IndexOf('(');
+                lastVarNameIndex = lines[i].IndexOf(')');
+                varName = lines[i].Substring(startVarNameIndex, lastVarNameIndex);  // 변수 찾기
+
+                startValueIndex = lines[i].IndexOf(':');
+                lastValueIndex = lines[i].IndexOf(',');
+                valueName = lines[i].Substring(startValueIndex, lastValueIndex);    // 변수 값 찾기
+
+                varNameDic.Add(varName, valueName);
+
+                dataDic.Add(i, varNameDic);
+                Debug.Log(dataDic[i]);
+                Debug.Log(varNameDic[varName]);
+            }
+        }
+        // 현재 JsonConvertData 안의 dataDic 딕셔너리에서 -> 배열인덱스(int), 변수(string), 변수값(string) 으로 들고있음
+        // 리턴값은 아무거나 넣어놓음
+        return JsonUtility.FromJson<T>(textAssetData);
     }
+
+    *//*// 아직 미사용중 TODO (사용안하면 지우기)
+    private void ParseVariableName(string variableName, string value)
+    {
+        if (variableName == "int")
+        {
+            int.Parse(value);
+        }
+        else if (variableName == "float")
+        {
+            float.Parse(value);
+        }
+        else if (variableName == "string")
+        {
+            value = value as string;
+        }
+
+    }*/
+
+
     #endregion
 }
