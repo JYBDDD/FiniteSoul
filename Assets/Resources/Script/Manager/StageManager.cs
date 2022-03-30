b463409eb6d459e8168bd4e9ef14ef1e87687b90
JYBDDD <98305534+JYBDDD@users.noreply.github.com>
2022-03-30 09:47:21 +0900

    몬스터 스폰위치 설정
    ScriptableObject

--- Assets/Resources/Script/Manager/StageManager.cs
+++ Assets/Resources/Script/Manager/StageManager.cs
@@ -20,7 +20,15 @@ public class StageManager : Singleton<StageManager>
     /// </summary>
     public void MonsterSpawn()
     {
+        // 만약 현재 BuildScene 과 stageIndex값이 동일하다면 실행
+        if(MonsterSpawnsDoc)
+        {
+            // ScriptableObject 에서 값을 추출할수 있는지 찾아봐야할것 TODO
+        }
+
+
         // 몬스터 생성위치를 ScriptableObject 로 만들어 넣어주도록 하자 (짜피 실행중 값 안바꿀거니까)  TODO
+        var monsterDatas = GameManager.Instance.FullData.monstersData;
     }
 
     /// <summary>
@@ -30,16 +38,16 @@ public class StageManager : Singleton<StageManager>
     {
         // 시작씬에서 캐릭터를 선택하였다면 선택한 캐릭터 인덱스를 SaveData로 저장한다음 불러오는것이 좋을듯 TODO
         // SaveData의 index (캐릭터 인덱스) 에 따라 캐릭터 생성 TODO  (일단은 팔라딘으로 설정함)
-
-        GameObject player = ResoureUtil.InsertPrefabs("Player/Paladin");
-        var volData = ResoureUtil.LoadSaveFile().playerVolatility;
+        var loadFile = ResoureUtil.LoadSaveFile();
+        GameObject player = ResoureUtil.InsertPrefabs(loadFile.resourcePath);
+        var volData = loadFile.playerVolatility;
 
         // 해당 플레이어를 0,0,0 위치에서 생성
         player.transform.position = new Vector3(volData.posX, volData.posY, volData.posZ);
 
         // 위에 TODO 수정후 Archer 일경우도 수정 ㄱㄱ TODO
         Paladin paladin = player.GetComponent<Paladin>();
-        paladin.playerData = ResoureUtil.LoadSaveFile();
+        paladin.playerData = loadFile;
         paladin.Initialize(paladin);
         paladin.SetStat();
 
--- Assets/Resources/Script/Util/ResourceUtil.cs
+++ Assets/Resources/Script/Util/ResourceUtil.cs
@@ -157,12 +157,12 @@ public class ResoureUtil : MonoBehaviour
     }
 
     /// <summary>
-    /// 프리팹 폴더에서 가져올 프리팹(즉시 생성)
+    /// Resources 폴더에서 가져올 프리팹(즉시 생성)
     /// </summary>
     /// <param name="path"></param>
     public static GameObject InsertPrefabs(string path)
     {
-        return Instantiate(Resources.Load<GameObject>($"Prefabs/{path}"));
+        return Instantiate(Resources.Load<GameObject>(path));
     }
 
     /// <summary>
