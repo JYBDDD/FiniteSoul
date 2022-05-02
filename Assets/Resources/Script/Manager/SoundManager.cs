502e9550bedae748688948ab697d69d027966dfe
JYBDDD <98305534+JYBDDD@users.noreply.github.com>
2022-05-02 11:18:56 +0900

    사운드 매니저 메서드 작성

--- Assets/Resources/Script/Manager/SoundManager.cs
+++ Assets/Resources/Script/Manager/SoundManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,7 +16,7 @@ public class SoundManager : Singleton<SoundManager>
     /// <summary>
     /// 오디오 클립들을 가지고 있을 딕셔너리
     /// </summary>
-    Dictionary<string, Dictionary<AudioSource,AudioClip>> audioClips = new Dictionary<string, Dictionary<AudioSource, AudioClip>>();
+    Dictionary<string, AudioClip> audioClipDic = new Dictionary<string, AudioClip>();
 
     private void Start()
     {
@@ -34,27 +35,103 @@ public class SoundManager : Singleton<SoundManager>
                 audioSources[i].loop = true;
             }
         }
+
+        // 오디오 딕셔너리 설정
+        AudioClipInit();
+    }
+
+    /// <summary>
+    /// 오디오 플레이
+    /// </summary>
+    /// <param name="name">오디오파일 이름</param>
+    /// <param name="soundType">오디오타입 - BGM / Effect</param>
+    /// <param name="soundPlayType">오디오플레이 타입 - Single / Multi</param>
+    public void PlayAudio(string name,SoundType soundType,SoundPlayType soundPlayType)
+    {
+        // Effect 실행
+        if(soundType == SoundType.Effect)
+        {
+            AudioSource audioSource = audioSources[(int)SoundType.Effect];
+
+            // 하나의 클립 실행
+            if(soundPlayType == SoundPlayType.Single)
+            {
+                audioSource.clip = AudioOnShotFind(name);
+            }
+            // 여러개의 클립중 랜덤으로 한개의 클립 실행
+            else
+            {
+                var audioList = AudioMultiShotFind(name);
+                audioSource.clip = audioList[UnityEngine.Random.Range(0, audioList.Count)];
+            }
+
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+        // BGM 실행
+        else
+        {
+            AudioSource audioSource = audioSources[(int)SoundType.BGM];
+
+            audioSource.clip = AudioOnShotFind(name);
+            audioSource.Play();
+        }
+    }
+
+    /// <summary>
+    /// 딕셔너리에서 오디오 여러개를 랜덤 출력하려고 할시 해당된 모든 클립을 찾는 메서드
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    private List<AudioClip> AudioMultiShotFind(string name)
+    {
+        List<AudioClip> audioList = new List<AudioClip>();
+
+        foreach (KeyValuePair<string,AudioClip> audio in audioClipDic)
+        {
+            if(audio.Key.Contains(name))
+            {
+                audioList.Add(audio.Value);
+            }
+        }
+
+        return audioList;
+    }
+
+    /// <summary>
+    /// 딕셔너리에서 오디오 하나만 출력시킬 클립을 찾는 메서드
+    /// </summary>
+    private AudioClip AudioOnShotFind(string name)
+    {
+        foreach(KeyValuePair<string,AudioClip> audio in audioClipDic)
+        {
+            if(audio.Key.Contains(name))
+            {
+                return audio.Value;
+            }
+        }
+
+        return null;
     }
 
 
 
-
     /// <summary>
-    /// 오디오 클립이름으로 딕셔너리에 존재하는 값인지 찾음, 없을경우 딕셔너리에 클립 삽입
+    /// 오디오 클립 초기 딕셔너리 설정
     /// </summary>
     /// <param name="clipName"></param>
     /// <returns></returns>
-    /*public AudioClip FindAudioClip(string clipName)
+    private void AudioClipInit()
     {
-        // 해당 딕셔러니에  오디오 클립에 같은 이름이 없다면 삽입후 실행, 아니라면 그냥실행  TODO
+        AudioClip[] audioClips = Resources.LoadAll<AudioClip>("Art/Sound");
 
+        for(int i =0; i < audioClips.Length; ++i)
+        {
+            audioClipDic.Add(audioClips[i].name, audioClips[i]);
+        }
 
-
-
-    }*/
-
-
+    }
 }
+
 
 /// <summary>
 /// 사운드 타입 (브금, 이펙트)
@@ -63,4 +140,13 @@ public enum SoundType
 {
     BGM,
     Effect,
+}
+
+/// <summary>
+/// 실행시킬 타입 (한개, 여러게 - 랜덤)
+/// </summary>
+public enum SoundPlayType
+{
+    Single,
+    Multi,
 }
